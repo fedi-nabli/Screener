c3e74bd13fce8ac68dfe328e277512d27a7ce9c5
fedi-nabli <fedinabli@gmail.com>
2025-03-30 15:20:36 +0200

    Initial Commit

---
+++ Screener/ScreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenerApp.swift
+//  Screener
+//
+//  Created by Fedi Nabli on 30/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
